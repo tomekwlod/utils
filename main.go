02c0d7e749a941e77a4f6f5f5a405c928445a458
Tomasz Wlodarczyk <twl@phase-ii.com>
2018-01-24 14:28:30 +0000

    AskConfirmation & StructToMap functions

--- main.go
+++ main.go
@@ -2,21 +2,54 @@ package utils
 
 import (
 	"archive/tar"
+	"bufio"
 	"compress/gzip"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 	"regexp"
 	"strings"
 	"sync"
+
+	env "github.com/segmentio/go-env"
 )
+
+// AskForConfirmation asks the user for confirmation. A user must type in "yes" or "no" and
+// then press enter. It has fuzzy matching, so "y", "Y", "yes", "YES", and "Yes" all count as
+// confirmations. If the input is not recognized, it will ask again. The function does not return
+// until it gets a valid response from the user.
+func AskForConfirmation(s string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [y/n]: ", s)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" {
+			return false
+		}
+	}
+}
+
+func EnvVariable(exportedName string) (variable string, err error) {
+	variable, err = env.Get(exportedName)
+	return
+}
 
 // FilesFromDirectory returns a list of files in passed directory.
 // If mustCompile variable passed, only the files that pass the regexp will be returned
-func FilesFromDirectory(directory string, mustCompile string) []string {
-	files := []string{}
-
+func FilesFromDirectory(directory string, mustCompile string) (files []string) {
 	filepath.Walk(directory+"/", func(path string, f os.FileInfo, err error) error {
 		if f.IsDir() == true {
 			return nil
@@ -34,7 +67,7 @@ func FilesFromDirectory(directory string, mustCompile string) []string {
 		return nil
 	})
 
-	return files
+	return
 }
 
 // Untar function unpacking an archive to destination directory
@@ -43,6 +76,7 @@ func Untar(path string, destinationpath string) error {
 	if destinationpath != "" {
 		os.RemoveAll(destinationpath)
 	}
+	os.Mkdir(destinationpath, 0700)
 
 	f, err := os.Open(path)
 	if err != nil {
@@ -105,6 +139,49 @@ func Untar(path string, destinationpath string) error {
 	}
 
 	return nil
+}
+
+func StructToMap(entry interface{}) (retval map[string]interface{}) {
+	s := reflect.ValueOf(entry)
+
+	typeOfT := s.Type()
+	if typeOfT.Kind() == reflect.Ptr {
+		typeOfT = typeOfT.Elem()
+	}
+
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		// uncomment below to see what's really happening
+		// fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
+		retval[typeOfT.Field(i).Name] = f.Interface()
+	}
+
+	return
+}
+
+// SlicesDiff comparef two slices together and returns two slices of the difference
+// The first slice contains what's been removed, opposite to the other slice
+// Note: First passed slice should be the old/previous/base one, second the new/current one
+func SlicesDiff(oldSlice, newSlice []string) (removed, added []string) {
+	for _, o := range oldSlice {
+		found := false
+
+		for i, n := range newSlice {
+			if n == o {
+				found = true
+				newSlice = append(newSlice[:i], newSlice[i+1:]...)
+				break
+			}
+		}
+
+		if !found {
+			removed = append(removed, o)
+		}
+	}
+
+	added = newSlice
+
+	return
 }
 
 // Below example shows how to execute many goroutines and wait until they are all done
