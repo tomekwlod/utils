a9e9c17b26284a98790b4cae20ee76740d2bd0f8
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2020-10-07 10:56:01 +0100

    is[upper/lower]

--- strings/main.go
+++ strings/main.go
@@ -54,3 +54,27 @@ func SplitString(str string) []string {
 
 	return strings.Fields(str)
 }
+
+func IsUpper(s string) bool {
+
+	for _, r := range s {
+
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+
+			return false
+		}
+	}
+	return true
+}
+
+func IsLower(s string) bool {
+
+	for _, r := range s {
+
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+
+			return false
+		}
+	}
+	return true
+}
